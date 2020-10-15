cc606fbbc7a651d5cf3a625163329e42eb642f61
Petr Kohout <xkohou14@stud.fit.vutbr.cz>
2020-10-15 22:22:31 +0200

    Updates

---
+++ precache-manifest.c038fd6cbd3f2555ac0699d926898c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff47082ed4e73585499b0a0a6a2b69ad",
+    "url": "/igem_team_seeker_client/index.html"
+  },
+  {
+    "revision": "35f6d0cf593a6c9cfec1",
+    "url": "/igem_team_seeker_client/static/css/main.581217f2.chunk.css"
+  },
+  {
+    "revision": "b31ac5adc54ff6cc2234",
+    "url": "/igem_team_seeker_client/static/js/2.cd555bce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/igem_team_seeker_client/static/js/2.cd555bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f6d0cf593a6c9cfec1",
+    "url": "/igem_team_seeker_client/static/js/main.f556425c.chunk.js"
+  },
+  {
+    "revision": "074eb71f03dacc7a272d",
+    "url": "/igem_team_seeker_client/static/js/runtime-main.911f62a4.js"
+  }
+]);
